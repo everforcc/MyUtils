211e3227aa53b4d076b9416bdd0b6c91e55aeee3
everforcc <718497737@qq.com>
2021-03-05 16:30:26 +0800

    线程池

---
+++ src/main/java/cn/cc/core/thread/NewCachedThreadPoolUtils.java
@@ -0,0 +1,56 @@
+package cn.cc.core.thread;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * @author c.c.
+ * @date 2021/3/1
+ */
+public class NewCachedThreadPoolUtils {
+
+    public static void main(String[] args) {
+        // 创建一个可缓存线程池
+        ExecutorService cachedThreadPool = Executors.newCachedThreadPool();
+        for (int i = 0; i < 10; i++) {
+            System.out.println("i:" + i);
+            try {
+                // sleep可明显看到使用的是线程池里面以前的线程，没有创建新的线程
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            cachedThreadPool.execute(new Runnable() {
+                public void run() {
+                    // 打印正在执行的缓存线程信息
+                    System.out.println(Thread.currentThread().getName()
+                            + "正在被执行");
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
+    }
+
+    //  传入需要的参数
+    public static void excute(Runnable runnable){
+        // 创建一个可缓存线程池
+        ExecutorService cachedThreadPool = Executors.newCachedThreadPool();
+
+        // for循环感觉不需要
+        for (int i = 0; i < 10; i++) { // 线程池为无限大，当执行当前任务时上一个任务已经完成，会复用执行上一个任务的线程，而不用每次新建线程
+            System.out.println("i:" + i);
+            try {
+                // sleep可明显看到使用的是线程池里面以前的线程，没有创建新的线程
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            cachedThreadPool.execute(runnable);
+        }
+    }
+
+}
