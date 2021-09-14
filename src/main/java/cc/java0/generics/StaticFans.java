b0f9039a186a0c47821cfaaa0f07e377ad010210
guokailong <gkl_cc@163.com>
2021-09-14 21:04:06 +0800

    20210914 整理文档

--- src/main/java/cc/java0/generics/StaticFans.java
+++ src/main/java/cc/java0/generics/StaticFans.java
@@ -1,7 +1,31 @@
 package cc.java0.generics;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
+ * 4. 泛型函数定义及使用
  * @author guokailong 2021-09-13
  */
+@Slf4j
 public class StaticFans {
+    //静态函数
+    public static  <T> void StaticMethod(T a){
+        log.info("StaticMethod: "+a.toString());
+    }
+    //普通函数
+    public  <T> void OtherMethod(T a){
+        log.info("OtherMethod: "+a.toString());
+    }
+}
+class StaticFansT{
+    public static void main(String[] args) {
+        //静态方法
+        StaticFans.StaticMethod("adfdsa");//使用方法一
+        StaticFans.<String>StaticMethod("adfdsa");//使用方法二
+
+        //常规方法
+        StaticFans staticFans = new StaticFans();
+        staticFans.OtherMethod(new Integer(123));//使用方法一
+        staticFans.<Integer>OtherMethod(new Integer(123));//使用方法二
+    }
 }
--- src/main/java/cc/java8/java8/Java8Stream/sort/StuDto.java
+++ src/main/java/cc/java8/java8/Java8Stream/sort/StuDto.java
@@ -3,8 +3,7 @@ package cc.java8.java8.Java8Stream.sort;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
@@ -30,13 +29,84 @@ public class StuDto implements Comparable<StuDto>  {
     }
 }
 class StuDoTest{
+    private static StuDto a = new StuDto(1,"a");
+    private static StuDto b = new StuDto(2,"b");
+    private static StuDto c = new StuDto(3,"c");
     public static void main(String[] args) {
+
+        //2.
+        //listSort();
+        //3.
+        //setSort();
+        //4.
+        mapSort();
+    }
+
+    private static void mapSort(){
+        Map<Integer, String> map = new HashMap<>();
+        map.put(1, "c");
+        map.put(2, "b");
+        map.put(3, "a");
+
+        System.out.println("---Value排序---");
+        map.entrySet().stream().sorted(Comparator.comparing(Map.Entry::getValue))
+                .forEach(e -> System.out.println("Key: "+ e.getKey() +", Value: "+ e.getValue()));
+
+        System.out.println("---Key排序---");
+        map.entrySet().stream().sorted(Comparator.comparing(Map.Entry::getKey))
+                .forEach(e -> System.out.println("Key: "+ e.getKey() +", Value: "+ e.getValue()));
+    }
+
+    //3. set
+    private static void setSort(){
+        Set<StuDto> set = new HashSet<>();
+        set.add(a);
+        set.add(b);
+        set.add(c);
+
+        System.out.println(" 定义顺序--- ");
+        set.stream().sorted().forEach(e ->
+                System.out.println("Id:" + e.getId() + ", Name: " + e.getName()));
+
+        System.out.println(" 定义反序--- ");
+        set.stream().sorted(Comparator.reverseOrder()).forEach(e ->
+                System.out.println("Id:" + e.getId() + ", Name: " + e.getName()));
+
+        System.out.println(" 未定义顺序--- ");
+        set.stream().sorted(Comparator.comparing(StuDto::getName)).forEach(e ->
+                System.out.println("Id:" + e.getId() + ", Name: " + e.getName()));
+
+        System.out.println(" 未定义反序--- ");
+        set.stream().sorted(Comparator.comparing(StuDto::getName).reversed()).forEach(e ->
+                System.out.println("Id:" + e.getId() + ", Name: " + e.getName()));
+
+    }
+
+    // 1.语法
+    // 2.list
+    private static void listSort(){
         List<StuDto> list = new ArrayList<>();
-        list.add(new StuDto(1, "Mahesh"));
-        list.add(new StuDto(2, "Suresh"));
-        list.add(new StuDto(3, "Nilesh"));
+        list.add(a);
+        list.add(b);
+        list.add(c);
+
+        System.out.println(" 定义顺序--- ");
         List<StuDto> slist = list.stream().sorted().collect(Collectors.toList());
         slist.forEach(e -> System.out.println("Id:" + e.getId() + ", Name: " + e.getName()));
 
+        System.out.println(" 定义反序--- ");
+        slist = list.stream().sorted(Comparator.reverseOrder()).collect(Collectors.toList());
+        slist.forEach(e -> System.out.println("Id:" + e.getId() + ", Name: " + e.getName()));
+
+        System.out.println(" 未定义顺序--- ");
+        slist = list.stream().sorted(Comparator.comparing(StuDto::getName)).collect(Collectors.toList());
+        slist.forEach(e -> System.out.println("Id:" + e.getId() + ", Name: " + e.getName()));
+
+        System.out.println(" 未定义反序--- ");
+        slist = list.stream().sorted(Comparator.comparing(StuDto::getName).reversed()).collect(Collectors.toList());
+        slist.forEach(e -> System.out.println("Id:" + e.getId() + ", Name: " + e.getName()));
     }
+
+
+
 }
