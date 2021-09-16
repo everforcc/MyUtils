c95ec1758cede9d784eb2568df2368839cfd932f
guokailong <gkl_cc@163.com>
2021-09-16 16:03:43 +0800

    20210916 整理文档

--- src/main/java/cc/java0/reflection/base/A.java
+++ src/main/java/cc/java0/reflection/base/A.java
@@ -4,11 +4,16 @@ package cc.java0.reflection.base;
  * @author guokailong 2021-09-09
  */
 public class A {
+
+    public String publicProperty;
+    private String privateProperty;
+
     private void methodA(String str){
         System.out.println("methodA : " + str);
     }
     public void methodB(String str){
         System.out.println("methodB : " + str);
     }
+
 }
 
