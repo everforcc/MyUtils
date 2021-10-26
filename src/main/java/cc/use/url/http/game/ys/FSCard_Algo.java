983f3369b8655bfd761b7d49c4ee3fd117ae1e1e
guokailong <gkl_cc@163.com>
2021-10-26 18:57:21 +0800

    20211026 整理文档

---
+++ src/main/java/cc/use/url/http/game/ys/FSCard_Algo.java
@@ -0,0 +1,24 @@
+package cc.use.url.http.game.ys;
+
+import java.math.BigDecimal;
+import java.util.*;
+
+/**
+ * @author guokailong 2021-10-26
+ */
+public class FSCard_Algo {
+
+    public static void main(String[] args) {
+        // 假设出up概率为x，不出1-x，但是下次必出
+        // 上次50%出，50%没出， 50% + 100%
+        // 0.5 + 0.25
+
+
+
+    }
+
+}
+
+
+
+
